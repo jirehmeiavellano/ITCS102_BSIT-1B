699b0d2d1e5f247cc50026e1665a2723a8f5ca3d
unknown <jirehmeiavellano@gmail.com>
2025-08-12 22:51:34 +0800

    initial commit

---
+++ activity3.py
@@ -0,0 +1,5 @@
+#print("Magandang Hapon \n\n\t\t\tBSIT-1B")
+
+print("The Quick Brown \fJumps Over the Lazy Dog.!")
+
+print("\"Ikaw lang sapat na\"")
---
+++ activity5.py
@@ -0,0 +1,3 @@
+name = eval("Type any character here..."))
+
+print("The data type of name is ", type(name))
